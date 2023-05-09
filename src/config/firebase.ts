f831ab45cdf89e2cdfcf1dc4b0ceeb602080c60d
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2023-05-09 15:05:53 +0100

    Update firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,9 +5,9 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAzTenDAG0vTX4f2eS_7rZqI8tVB4_sj88",
-  authDomain: "minder-105e5.firebaseapp.com",
-  projectId: "minder-105e5",
-  storageBucket: "minder-105e5.appspot.com",
+  authDomain: "quotella-105e5.firebaseapp.com",
+  projectId: "quotella-105e5",
+  storageBucket: "quotella-105e5.appspot.com",
   messagingSenderId: "215921657458",
   appId: "1:215921657458:web:bbebcf640ded51ef8564eb",
   measurementId: "G-0N02TGVR9N",
