03a3eb01300ee4d59d0959b188b25864eaabbf45
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2023-06-21 13:21:42 +0100

    Under Construction

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { UserContext } from "./contexts/UserContext";
 import { AuthContext } from "./contexts/Auth";
 import Profile from "./pages/profile";
 import Verification from "./pages/verification";
+import UnderConstruction from "./components/UnderConstruction";
 
 const App: React.FC = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
@@ -51,23 +52,24 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <main className="w-screen overflow-x-hidden">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage isMobile={isMobile} />} />
-          <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/register" element={<Register />} />
+    // <main className="w-screen overflow-x-hidden">
+    //   <Router>
+    //     <Routes>
+    //       <Route path="/" element={<LandingPage isMobile={isMobile} />} />
+    //       <Route path="/auth/login" element={<Login />} />
+    //       <Route path="/auth/register" element={<Register />} />
 
-          {/* After auth routes */}
+    //       {/* After auth routes */}
 
-          <Route path="/minder/home" element={<Home />} />
-          <Route path="/minder/addpost" element={<AddPost />} />
-          <Route path="/minder/search" element={<Search />} />
-          <Route path="/minder/user/profile" element={<Profile />} />
-          <Route path="/minder/user/verification" element={<Verification />} />
-        </Routes>
-      </Router>
-    </main>
+    //       <Route path="/minder/home" element={<Home />} />
+    //       <Route path="/minder/addpost" element={<AddPost />} />
+    //       <Route path="/minder/search" element={<Search />} />
+    //       <Route path="/minder/user/profile" element={<Profile />} />
+    //       <Route path="/minder/user/verification" element={<Verification />} />
+    //     </Routes>
+    //   </Router>
+    // </main>
+    <UnderConstruction />
   );
 };
 
---
+++ src/components/UnderConstruction.tsx
@@ -0,0 +1,13 @@
+//@ts-ignore
+import React from "react";
+
+const UnderConstruction = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1>MINDER UNDER CONSTRUCTION 🚧</h1>
+      <span className="text-sm">Comeback in 2 days</span>
+    </div>
+  );
+};
+
+export default UnderConstruction;
